ec89c4bf0d803566f2e533a871f697eeffddeb59
ens20mlr <104353331+ens20mlr@users.noreply.github.com>
2025-02-05 13:02:31 +0100

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -5,11 +5,11 @@ const meta = {
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I'm Alva",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "I love Architecture",
+        second: "hoho",
+        third: "hihi",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
